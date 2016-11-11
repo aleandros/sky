6db975d859643576b81c8f18595ea7be3b14b382
Edgar Cabrera <edgar@cafeinacode.com>
2016-11-10 22:39:11 -0600

    Modify mix.exs for publishing in hex

--- mix.exs
+++ mix.exs
@@ -7,26 +7,32 @@ defmodule Sky.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
+     description: description,
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, "~> 0.14", only: :dev}]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Edgar Cabrera"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/aleandros/sky",
+               "Docs" => "http://hexdocs.pm/sky/"}
+    ]
+  end
+
+  defp description do
+    """
+    Set of functions for manipulating other functions in elixir.
+    """
   end
 end
