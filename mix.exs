3bc354afbbbcebfe3e926615cccb5d7b7b50554c
Edgar Cabrera <edgar@cafeinacode.com>
2016-11-12 00:25:20 -0600

    Change patch version - correction to curry (thanks to vic)
    Thanks to Víctor Borja for his contribution in formatting
    and finding this very important case!

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sky.Mixfile do
 
   def project do
     [app: :sky,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
